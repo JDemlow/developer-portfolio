7eba5893a5f92397cea56e8c1efe58fd491169e7
James Demlow <jdemlow@gmail.com>
2023-04-02 19:47:35 -0600

    editing Computers.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Adrian &nbsp;
-            <span className="sm:block hidden"> | JavaScript Mastery</span>
+            James Demlow &nbsp;
+            <span className="sm:block hidden"> | Front-End Developer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
